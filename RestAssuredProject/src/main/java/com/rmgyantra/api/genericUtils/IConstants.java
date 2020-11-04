0737da401c00151c1f261943e29c5715c4ec55e4
Deepak <Deepak@Ty-Deepak-Tr>
2020-11-04 17:09:38 +0530

    modified

--- RestAssuredProject/src/main/java/com/rmgyantra/api/genericUtils/IConstants.java
+++ RestAssuredProject/src/main/java/com/rmgyantra/api/genericUtils/IConstants.java
@@ -2,7 +2,7 @@ package com.rmgyantra.api.genericUtils;
 
 public interface IConstants {
 	
-	String DBURL = "";
+	String DBURL = "jdbc:mysql://localhost:3306/projects";
 	String DBUSername = "root";
 	String DBPAssword = "root";
 
